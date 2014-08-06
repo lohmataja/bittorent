e5eacddb262077197aa3b8baae2469ef298e6e8e
lohmataja <lohmataja@gmail.com>
2014-08-06 14:39:46 -0400

    Removed bencoding, fixed references to it.

--- torrent.py
+++ torrent.py
@@ -1,96 +1,45 @@
-import collections
+import bencoding
 import hashlib
 import requests
 from bitstring import BitArray
 import socket
 import struct
 
-
 MAX_REQUESTS = 2
-
-def bdecode(benstr):
-    """takes a bencoded string
-    returns a dictionary"""
-    def bdecode_string(benstr, i):
-        j = benstr.find(':', i)
-        str_len = int(benstr[i:j])
-        end = j+1+str_len
-        return benstr[j+1:end], end
-
-    def bdecode_int(benstr, i):
-        j = benstr.find('e', i)
-        return int(benstr[i:j]), j+1
-
-    def bdecode_list(benstr, i):
-        res = []
-        while benstr[i] != 'e':
-            new_el, i = bdecode_element(benstr, i)
-            res.append(new_el)
-        return res, i+1
-
-    def bdecode_dict(benstr, i):
-        res = collections.OrderedDict()
-        while i < len(benstr) and benstr[i] != 'e':
-            new_key, i = bdecode_element(benstr, i)
-            new_value, i = bdecode_element(benstr, i)
-            res[new_key] = new_value
-        return res, i+1
-
-    def bdecode_element(benstr, i):
-        dispatch = {'i':bdecode_int, 'l':bdecode_list, 'd':bdecode_dict}
-        return dispatch.get(benstr[i], bdecode_string)(benstr, i+(benstr[i] in 'ild'))
-
-    return bdecode_element(benstr, 0)[0]
-
-def bencode(item):
-    def bencode_int(i):
-        return 'i'+str(i)+'e'
-
-    def bencode_list(l):
-        return 'l'+''.join([bencode_item(li) for li in l])+'e'
-
-    def bencode_str(s):
-        return str(len(s))+':'+s
-
-    def bencode_dict(d):
-        return 'd'+''.join([bencode_item(key)+bencode_item(value) for key, value in d.items()])+'e'
-
-    def bencode_item(item):
-        dispatch = {int:bencode_int, list:bencode_list, str:bencode_str}
-        return dispatch.get(type(item), bencode_dict)(item)
-
-    return bencode_item(item)
-
+MAX_CONNECTIONS = 2
 
 class Torrent():
 
     def __init__(self, torrent_file):
-        #TODO: decide on data structure to keep track of pieces
-        self.torrent_data = bdecode(open(torrent_file, 'rb').read())
+        self.torrent_data = bencoding.bdecode(open(torrent_file, 'rb').read())
         self.announce = self.torrent_data['announce']
         self.info = self.torrent_data['info']
-        self.info_hash = hashlib.sha1(bencode(self.torrent_data['info'])).digest()
+        self.info_hash = hashlib.sha1(bencoding.bencode(self.torrent_data['info'])).digest()
         self.peer_id = 'liutorrent1234567890'
         self.uploaded = 0
         self.downloaded = 0
         self.port = 6881 #how do I choose a port? randomly within the unreserved range?
         self.filename = self.info['name'] #for now, single file only
 
+        #handshake: <pstrlen><pstr><reserved><info_hash><peer_id>
+        self.handshake = ''.join([chr(19), 'BitTorrent protocol', chr(0)*8, self.info_hash, self.peer_id])
+
         self.length = self.info['length'] if 'length' in self.info.keys() \
             else sum([f['length'] for f in self.info['files']])
         self.piece_len = self.info['piece length']
         self.BLOCK_LEN = 2**14
-        self.num_blocks = self.piece_len / self.BLOCK_LEN
         self.num_pieces = self.length/self.piece_len + 1
         self.last_piece_len = self.length % self.piece_len
         self.last_block_len = self.piece_len % self.BLOCK_LEN
+        self.blocks_per_piece = self.piece_len / self.BLOCK_LEN + 1 * (self.last_block_len != 0)
+        #TODO: piece/block representation
         self.pieces = BitArray(bin='0'*self.num_pieces)
 
         self.info_from_tracker = self.update_info_from_tracker()
         self.peers = self.get_peers()
 
         self.num_connected = 0
-        self.MAX_CONNECTIONS = 2
+        self.MAX_CONNECTIONS = MAX_CONNECTIONS
         self.requests = []
 
     def get_left(self):
@@ -108,7 +57,7 @@ class Torrent():
 
     def update_info_from_tracker(self):
         tracker_response = requests.get(self.announce, params=self.get_params())
-        return bdecode(tracker_response.text)
+        return bencoding.bdecode(tracker_response.text)
 
     def get_peers(self):
         peer_bytes = [ord(byte) for byte in self.info_from_tracker['peers']]
